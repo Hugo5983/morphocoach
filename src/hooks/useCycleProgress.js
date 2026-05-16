be8129b0dc699be9d065007f5dd1bbe7c3204260
Hugo5983 <hugo.savarino59@gmail.com>
2026-05-16 14:14:23 +0200

    Delete src directory

--- src/hooks/useCycleProgress.js
+++
@@ -1,6 +0,0 @@
-import { useMemo } from "react";
-import { getCycleProgress } from "../services/workoutService.js";
-
-export function useCycleProgress(cycleStart) {
-  return useMemo(() => getCycleProgress(cycleStart), [cycleStart]);
-}
