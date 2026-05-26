8913c3e71f1731074b9c97f1c0fd96786f26f90c
Hugo5983 <hugo.savarino59@gmail.com>
2026-05-26 10:32:53 +0200

    Delete src directory

--- src/hooks/useStreak.js
+++
@@ -1,6 +0,0 @@
-import { useMemo } from "react";
-import { computeStreak } from "../services/workoutService.js";
-
-export function useStreak(prog) {
-  return useMemo(() => computeStreak(prog), [prog]);
-}
