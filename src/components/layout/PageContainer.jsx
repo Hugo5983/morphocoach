f42f5854396768dd9868a40e49ed9da8742f8336
Hugo5983 <hugo.savarino59@gmail.com>
2026-05-27 09:39:31 +0200

    Delete src directory

--- src/components/layout/PageContainer.jsx
+++
@@ -1,7 +0,0 @@
-export function PageContainer({ children, style }) {
-  return (
-    <main style={{ maxWidth: 500, margin: "0 auto", paddingBottom: 72, ...style }}>
-      {children}
-    </main>
-  );
-}
