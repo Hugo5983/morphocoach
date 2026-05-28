7aece6888bfc5ae8504ea04315577b86d05431cd
Hugo5983 <hugo.savarino59@gmail.com>
2026-05-28 22:26:19 +0200

    Update PageContainer.jsx

--- src/components/layout/PageContainer.jsx
+++ src/components/layout/PageContainer.jsx
@@ -1,6 +1,11 @@
 export function PageContainer({ children, style }) {
   return (
-    <main style={{ maxWidth: 500, margin: "0 auto", paddingBottom: 72, ...style }}>
+    <main style={{
+      maxWidth: 500,
+      margin: "0 auto",
+      paddingBottom: "calc(84px + env(safe-area-inset-bottom, 0px))",
+      ...style
+    }}>
       {children}
     </main>
   );
